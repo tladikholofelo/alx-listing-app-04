b7c8c452ed227366e6c913c8cf2aea15dd2c1990
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2025-02-02 16:07:45 +0200

    Create pages/booking/index.tsx

---
+++ pages/booking/index.tsx
@@ -0,0 +1,28 @@
+import BookingForm from '@/components/booking/BookingForm';
+import OrderSummary from '@/components/booking/OrderSummary';
+
+export default function BookingPage() {
+  const bookingDetails = {
+    propertyName: "Villa Arrecife Beach House",
+    price: 7500,
+    bookingFee: 65,
+    totalNights: 3,
+    startDate: "24 August 2024",
+  };
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        {/* Main Form Section */}
+        <div className="lg:col-span-2">
+          <BookingForm />
+        </div>
+
+        {/* Order Summary Sidebar */}
+        <div className="lg:col-span-1">
+          <OrderSummary bookingDetails={bookingDetails} />
+        </div>
+      </div>
+    </div>
+  );
+}
